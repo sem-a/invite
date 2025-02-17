ec89a297056af0e82173e7f6d5fa6acae352b0f1
sem-a <aleksey.i99@mail.ru>
2025-02-18 00:00:03 +0300

    fasdfasdf

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const logger = require('morgan');
 const cors = require("cors");
 const path = require("path");
 
